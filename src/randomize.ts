9032ce430e7ae2fd28dcdc64e4475eea9ae47f58
RJ <rj@rjsmith.me>
2022-07-03 17:41:58 -0400

    Refactored if chain into switch statement.

--- src/randomize.ts
+++ src/randomize.ts
@@ -12,34 +12,32 @@ export default function randomize(item?: string): string {
   const itemTier = Math.floor(Math.random() * 5) + 4;
   let itemType = "";
 
-  if (item === "twoHandedWeapon") {
-    itemType =
-      twoHandedWeapons[Math.floor(Math.random() * twoHandedWeapons.length)];
-  }
-
-  if (item === "oneHandedWeapon") {
-    itemType =
-      oneHandedWeapons[Math.floor(Math.random() * oneHandedWeapons.length)];
-  }
-
-  if (item === "armor") {
-    itemType = armors[Math.floor(Math.random() * armors.length)];
-  }
-
-  if (item === "helmet") {
-    itemType = helmets[Math.floor(Math.random() * helmets.length)];
-  }
-
-  if (item === "boots") {
-    itemType = boots[Math.floor(Math.random() * boots.length)];
-  }
-
-  if (item === "capes") {
-    itemType = capes[Math.floor(Math.random() * capes.length)];
-  }
-
-  if (item === "offhands") {
-    itemType = offhands[Math.floor(Math.random() * offhands.length)];
+  switch (item) {
+    case "twoHandedWeapon":
+      itemType =
+        twoHandedWeapons[Math.floor(Math.random() * twoHandedWeapons.length)];
+      break;
+    case "oneHandedWeapon":
+      itemType =
+        oneHandedWeapons[Math.floor(Math.random() * oneHandedWeapons.length)];
+      break;
+    case "offhands":
+      itemType = offhands[Math.floor(Math.random() * offhands.length)];
+      break;
+    case "capes":
+      itemType = capes[Math.floor(Math.random() * capes.length)];
+      break;
+    case "boots":
+      itemType = boots[Math.floor(Math.random() * boots.length)];
+      break;
+    case "armor":
+      itemType = armors[Math.floor(Math.random() * armors.length)];
+      break;
+    case "helmet":
+      itemType = helmets[Math.floor(Math.random() * helmets.length)];
+      break;
+    default:
+      break;
   }
 
   return `https://render.albiononline.com/v1/item/T${itemTier}_${itemType}${itemLevel}.png?count=1&quality=${itemQuality}`;
